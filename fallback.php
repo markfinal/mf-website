186ec21b1a42c4f06518935376cc4ce543854995
Mark Final <markfinal@hotmail.com>
2016-05-31 10:58:03 +0100

    Need to disable auto_globals_git for global vars like  to be instantiated at startup

--- fallback.php
+++ fallback.php
@@ -2,7 +2,9 @@
 header('Content-Type: application/json');
 $new_array = $GLOBALS;
 $index = array_search('GLOBALS',array_keys($new_array));
-$spliced_array = array_splice($new_array, $index, $index-1);
-$combined = array_merge($spliced_array, $_SERVER, $_REQUEST, $_SESSION, $_ENV);
-echo json_encode($combined, JSON_PRETTY_PRINT);
+echo json_encode(array_splice($new_array, $index, $index-1), JSON_PRETTY_PRINT);
+echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+echo json_encode($_REQUEST, JSON_PRETTY_PRINT);
+echo json_encode($_SESSION, JSON_PRETTY_PRINT);
+echo json_encode($_ENV, JSON_PRETTY_PRINT);
 ?>
