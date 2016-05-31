8d66835c9e69922452a4920b2810eac1010e08de
Mark Final <markfinal@hotmail.com>
2016-05-31 06:44:36 +0100

    Pretty print JSON?

--- fallback.php
+++ fallback.php
@@ -1,5 +1,5 @@
 <?php
 $new_array = $GLOBALS;
 $index = array_search('GLOBALS',array_keys($new_array));
-echo json_encode(array_splice($new_array, $index, $index-1));
+echo json_encode(array_splice($new_array, $index, $index-1), JSON_PRETTY_PRINT);
 ?>
