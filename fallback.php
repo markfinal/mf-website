5dff257be3c0408bb4d329d653108051a69cf429
Mark Final <markfinal@hotmail.com>
2016-05-31 07:58:12 +0100

    Return more PHP variables as JSON

--- fallback.php
+++ fallback.php
@@ -4,4 +4,7 @@ $new_array = $GLOBALS;
 $index = array_search('GLOBALS',array_keys($new_array));
 echo json_encode(array_splice($new_array, $index, $index-1), JSON_PRETTY_PRINT);
 echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+echo json_encode($_REQUEST, JSON_PRETTY_PRINT);
+echo json_encode($_SESSION, JSON_PRETTY_PRINT);
+echo json_encode($_ENV, JSON_PRETTY_PRINT);
 ?>
