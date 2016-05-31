ce2e85ac3e6e1abdd089a7435784d1c7d443536a
Mark Final <markfinal@hotmail.com>
2016-05-31 06:43:15 +0100

    Display all predefined variables as JSON?

--- fallback.php
+++ fallback.php
@@ -1,5 +1,5 @@
 <?php
-    $path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
-    $elements = explode('/', $path);                // Split path on slashes
-    echo json_encode($elements);
+$new_array = $GLOBALS;
+$index = array_search('GLOBALS',array_keys($new_array));
+echo json_encode(array_splice($new_array, $index, $index-1));
 ?>
