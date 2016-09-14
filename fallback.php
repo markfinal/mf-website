256642c5da059f44da371217b2e31f2dd8c2abb1
Mark Final <markfinal@hotmail.com>
2016-09-14 21:01:16 +0100

    send_email function now in it's own source file

--- fallback.php
+++ fallback.php
@@ -1,4 +1,6 @@
 <?php
+require 'send_email.php';
+
 register_shutdown_function( "fatal_handler" );
 
 function fatal_handler()
@@ -19,70 +21,6 @@ function fatal_handler()
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
     echo json_encode($errstr);
   }
-}
-
-function send_email($to, $subject, $message, $attachments=array())
-{
-	$from = 'Mark Final <mark@markfinal.me.uk>';
-
-	$boundary = uniqid('np');
-
-	// note: for both header and message body, the EOL characters must be enclosed in
-	// double quotes, in order for PHP to interpret these as carriage return and line feeds
-
-	/* this is an alternative multipart message
-	// the header
-	$header = array();
-	$header[] = 'MIME-Version: 1.0';
-	$header[] = 'Content-Type: multipart/alternative; boundary="'.$boundary.'"';
-	$header[] = 'From: '.$from;
-	$header[] = 'Reply-To: '.$from;
-	$header[] = 'X-Mailer: PHP/'.phpversion();
-
-	// the message body
-	$message = 'This is a MIME encoded message.'."\r\n\r\n";
-	$message .= '--'.$boundary."\r\n";
-	$message .= 'Content-type: text/plain;charset=utf-8'."\r\n";
-	$message .= 'Content-Transfer-Encoding: 7bit'."\r\n";
-	$message .= "\r\n";
-	$message .= 'Some plain text here'."\r\n\r\n";
-	$message .= '--'.$boundary."\r\n";
-	$message .= 'Content-Type: text/html;charset=utf-8'."\r\n";
-	$message .= 'Content-Transfer-Encoding: 7bit'."\r\n";
-	$message .= "\r\n";
-	$message .= '<html><body>Hello world</body></html>'."\r\n\r\n";
-	$message .= '--'.$boundary.'--';
-	*/
-	$header = array();
-	$header[] = 'MIME-Version: 1.0';
-	$header[] = 'Content-Type: multipart/mixed; boundary="'.$boundary.'"';
-	$header[] = 'From: '.$from;
-	$header[] = 'Reply-To: '.$from;
-	$header[] = 'X-Mailer: PHP/'.phpversion();
-
-	// the message body
-	$message = 'This is a MIME encoded message.'."\r\n\r\n";
-	$message .= '--'.$boundary."\r\n";
-	$message .= 'Content-type: text/plain;charset=utf-8'."\r\n";
-	$message .= 'Content-Transfer-Encoding: 7bit'."\r\n";
-	$message .= "\r\n";
-	$message .= $message."\r\n\r\n";
-	foreach ($attachments as $key => $value)
-	{
-		$message .= '--'.$boundary."\r\n";
-		$message .= 'Content-Type: text/plain;name="'.$key.'"'."\r\n";
-		$message .= 'Content-Transfer-Encoding: base64'."\r\n";
-		$message .= 'Content-Disposition: attachment'."\r\n";
-		$message .= "\r\n";
-		$content = chunk_split(base64_encode($value));
-		$message .= $content."\r\n\r\n";
-	}
-	$message .= '--'.$boundary.'--';
-
-	// actually send the mail
-	$mail_sent = mail($to, $subject, $message, implode("\r\n", $header), '-v');
-
-	error_log($mail_sent ? 'Mail was sent' : 'Mail failed, '.error_get_last());
 }
 
 function user_registration()
