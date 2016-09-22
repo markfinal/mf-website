6c89b79fcbe1210a71ed756e1e848894b5f3dbd4
Mark Final <markfinal@hotmail.com>
2016-09-22 07:05:00 +0100

    Calculate the absolute path to the DB password

--- api/v1/dbutils.php
+++ api/v1/dbutils.php
@@ -3,7 +3,8 @@ require_once 'api/v1/log.php';
 
 function connectdb()
 {
-    $password = explode("\n", file_get_contents('phppasswd'));
+    $path = realpath(__DIR__.'/../../phppasswd');
+    $password = explode("\n", file_get_contents($path));
 
     $connection = new PDO('mysql:host=localhost;dbname=markfina_entitlements;charset=utf8', 'markfina_php', $password[0]);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
