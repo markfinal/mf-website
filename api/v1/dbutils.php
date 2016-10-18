73774b3e28612fabdfdc6882093d302f1d9e8b24
Mark Final <markfinal@hotmail.com>
2016-10-18 11:53:12 +0100

    Moved SQL dump to a private area. Also the DB password file is in here (but not in git)

--- api/v1/dbutils.php
+++ api/v1/dbutils.php
@@ -3,7 +3,7 @@ require_once 'api/v1/log.php';
 
 function connectdb()
 {
-    $path = realpath(__DIR__.'/../../phppasswd');
+    $path = realpath(__DIR__.'/../../private/phppasswd');
     $password = explode("\n", file_get_contents($path));
 
     $connection = new PDO('mysql:host=localhost;dbname=markfina_entitlements;charset=utf8', 'markfina_php', $password[0]);
